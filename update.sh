156896f7a31f57aa40ea970a3db0cb0ca543ad4f
hotio <hotio@protonmail.com>
2019-11-16 10:02:07 +0100

    [CI SKIP] add failsafe

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 version=$(curl -fsSL "https://ci.appveyor.com/api/projects/tidusjar/requestplex/history?recordsNumber=100" | jq -r '. | first(.builds[] | select(.status == "success") | select(.branch =="develop") | select(.pullRequestId == null)) | .version')
+[[ -z ${version} ]] && exit
 jobid=$(curl -fsSL "https://ci.appveyor.com/api/projects/tidusjar/requestplex/build/${version}" | jq -r '.build.jobs[0].jobId')
+[[ -z ${jobid} ]] && exit
 find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG OMBI_VERSION=.*$/ARG OMBI_VERSION=${version}/g" {} \;
 find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG OMBI_JOBID=.*$/ARG OMBI_JOBID=${jobid}/g" {} \;
 sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
