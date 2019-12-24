60570f5220fd06c9d47d35d0858669cb993d6e23
hotio <hotio@protonmail.com>
2019-12-24 10:24:12 +0100

    [CI SKIP] exit 1 in update.sh

--- update.sh
+++ update.sh
@@ -46,9 +46,9 @@ elif [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM .*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile && echo "${digest}"
 else
     version=$(curl -fsSL "https://ci.appveyor.com/api/projects/tidusjar/requestplex/history?recordsNumber=100" | jq -r '. | first(.builds[] | select(.status == "success") | select(.branch =="develop") | select(.pullRequestId == null)) | .version')
-    [[ -z ${version} ]] && exit
+    [[ -z ${version} ]] && exit 1
     jobid=$(curl -fsSL "https://ci.appveyor.com/api/projects/tidusjar/requestplex/build/${version}" | jq -r '.build.jobs[0].jobId')
-    [[ -z ${jobid} ]] && exit
+    [[ -z ${jobid} ]] && exit 1
     find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG OMBI_VERSION=.*$/ARG OMBI_VERSION=${version}/g" {} \;
     find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG OMBI_JOBID=.*$/ARG OMBI_JOBID=${jobid}/g" {} \;
     sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
